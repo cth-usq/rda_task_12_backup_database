96d09d1f9a56fa8a0362922bd87564c614d960d2
cthusq <cthusq.qusevi@gmail.com>
2024-04-03 19:15:30 +0300

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,14 @@
 #! /bin/bash
+
+USER=$DB_USER
+PASSWORD=$DB_PASSWORD
+
+DATABASE="ShopDB"
+DB_RSERVE="ShopDBReserve"
+DB_DEVELOPMENT="ShopDBDevelopment"
+
+mysqldump -u"$USER" -p"$PASSWORD" "$DATABASE" --no-create-db --result-file=full-backup-db.sql
+mysql -u"$USER" -p"$PASSWORD" "$DB_RSERVE" < full-backup-db.sql
+
+mysqldump -u"$USER" -p"$PASSWORD" "$DATABASE" --no-create-info --result-file=data-backup-db.sql
+mysql -u"$USER" -p"$PASSWORD" "$DB_DEVELOPMENT" < data-backup-db.sql
